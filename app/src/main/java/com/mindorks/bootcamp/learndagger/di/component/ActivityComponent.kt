f05bafac6eb858bd74c8b6c91c3494807295f19c
sahilandroid19 <sahilandroid19@gmail.com>
2019-06-06 17:32:21 +0530

    Updated

--- app/src/main/java/com/mindorks/bootcamp/learndagger/di/component/ActivityComponent.kt
+++ app/src/main/java/com/mindorks/bootcamp/learndagger/di/component/ActivityComponent.kt
@@ -1,13 +1,12 @@
 package com.mindorks.bootcamp.learndagger.di.component
 
 import com.mindorks.bootcamp.learndagger.di.module.ActivityModule
-import com.mindorks.bootcamp.learndagger.di.module.ApplicationModule
 import com.mindorks.bootcamp.learndagger.di.scope.ActivityScope
 import com.mindorks.bootcamp.learndagger.ui.main.MainActivity
 import dagger.Component
 
 @ActivityScope
-@Component(dependencies = [ApplicationModule::class], modules = [ActivityModule::class])
+@Component(dependencies = [ApplicationComponent::class], modules = [ActivityModule::class])
 interface ActivityComponent {
 
     fun inject(mainActivity: MainActivity)
